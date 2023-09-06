cc01ddff2602f58adbaaffbb2b5735b7f03406cf
Yuto Urashima <yuto.urashima24@gmail.com>
2023-09-06 22:23:53 +0900

    productNameFormを作成

--- src/components/organisms/productForms/productNameForm/index.tsx
+++ src/components/organisms/productForms/productNameForm/index.tsx
@@ -1,3 +1,4 @@
+import { InputLabel, TextField, Typography } from '@mui/material'
 import { Controller, useFormContext } from 'react-hook-form'
 
 type ProductNameFormProps = {
@@ -15,7 +16,23 @@ const ProductNameForm = ({ name = 'productName' }: ProductNameFormProps) => {
                 rules={{ required: '商品名を入力してください' }}
                 defaultValue=""
                 render={({ field, fieldState }) => {
-                    return <>Hello</>
+                    return (
+                        <>
+                            <InputLabel htmlFor={name}>
+                                <Typography variant="h5">商品名</Typography>
+                            </InputLabel>
+                            <TextField
+                                {...field}
+                                id={name}
+                                type="text"
+                                variant="outlined"
+                                placeholder="ソファー"
+                                error={fieldState.invalid}
+                                helperText={fieldState.error?.message}
+                                fullWidth
+                            />
+                        </>
+                    )
                 }}
             />
         </>
