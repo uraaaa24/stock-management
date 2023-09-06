2ccb2c7310333664f5fb85ed679d2101d2de92c3
Yuto Urashima <yuto.urashima24@gmail.com>
2023-09-06 10:11:41 +0900

    Gridの導入

--- src/pages/order/create/index.tsx
+++ src/pages/order/create/index.tsx
@@ -1,4 +1,4 @@
-import { Paper } from "@mui/material"
+import { Grid, Paper, Typography } from "@mui/material"
 import { FormProvider, useForm } from "react-hook-form"
 
 const Create = () => {
@@ -6,7 +6,13 @@ const Create = () => {
 
     return (
         <FormProvider {...methods}>
-            <Paper elevation={4} sx={{maxWidth: 800, mx: "auto", my: 6}}>新規発注登録</Paper>
+            <Paper elevation={4} sx={{ maxWidth: 800, mx: "auto", my: 6 }}>
+                <Grid container component={"form"} rowSpacing={4} sx={{ justifyContent: "space-between", my: 0, mx: "auto", width: 400 }}>
+                    <Grid item xs={12}>
+                        <Typography variant="h4">新規発注登録</Typography>
+                    </Grid>
+                </Grid>
+            </Paper>
         </FormProvider>
     )
 }
