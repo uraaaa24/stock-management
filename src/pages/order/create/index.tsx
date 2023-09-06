9b02d0d0d2b6f75e430f2177b2269d1a857e16a7
Yuto Urashima <yuto.urashima24@gmail.com>
2023-09-06 10:03:54 +0900

    Paperで囲むように修正

--- src/pages/order/create/index.tsx
+++ src/pages/order/create/index.tsx
@@ -1,10 +1,13 @@
+import { Paper } from "@mui/material"
 import { FormProvider, useForm } from "react-hook-form"
 
 const Create = () => {
     const methods = useForm()
 
     return (
-        <FormProvider {...methods}>create</FormProvider>
+        <FormProvider {...methods}>
+            <Paper elevation={4} sx={{maxWidth: 800, mx: "auto", my: 6}}>新規発注登録</Paper>
+        </FormProvider>
     )
 }
 
