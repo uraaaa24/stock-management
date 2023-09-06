c769f58c95ad0c42a0b95ff8f8d820e53baac664
Yuto Urashima <yuto.urashima24@gmail.com>
2023-09-06 22:31:25 +0900

    submitボタンを追加

--- src/pages/order/create/index.tsx
+++ src/pages/order/create/index.tsx
@@ -1,16 +1,26 @@
-import ProductNameForm from "@/components/organisms/productForms/productNameForm"
-import { getDefaultLayout } from "@/components/templates/layout"
-import { NextPageWithLayout } from "@/pages/_app"
-import { Grid, Paper, Typography } from "@mui/material"
-import { FormProvider, useForm } from "react-hook-form"
+import ProductNameForm from '@/components/organisms/productForms/productNameForm'
+import { getDefaultLayout } from '@/components/templates/layout'
+import { NextPageWithLayout } from '@/pages/_app'
+import { Button, Grid, Paper, Typography } from '@mui/material'
+import { FormProvider, useForm } from 'react-hook-form'
 
 const Create: NextPageWithLayout = () => {
     const methods = useForm()
 
+    const onSubmit = (data: any) => {
+        console.log(data)
+    }
+
     return (
         <FormProvider {...methods}>
-            <Paper elevation={4} sx={{ maxWidth: 800, mx: "auto", my: 6 }}>
-                <Grid container component={"form"} rowSpacing={4} sx={{ justifyContent: "space-between", my: 0, mx: "auto", width: 400 }}>
+            <Paper elevation={4} sx={{ maxWidth: 800, mx: 'auto', my: 6 }}>
+                <Grid
+                    container
+                    component={'form'}
+                    rowSpacing={4}
+                    onSubmit={methods.handleSubmit(onSubmit)}
+                    sx={{ justifyContent: 'space-between', my: 0, mx: 'auto', width: 400 }}
+                >
                     <Grid item xs={12}>
                         <Typography variant="h4">新規発注登録</Typography>
                     </Grid>
@@ -19,6 +29,11 @@ const Create: NextPageWithLayout = () => {
                         <ProductNameForm name="productName" />
                     </Grid>
 
+                    <Grid item xs={12}>
+                        <Button type="submit" variant="outlined" size="large" sx={{ width: 200 }}>
+                            発注登録
+                        </Button>
+                    </Grid>
                 </Grid>
             </Paper>
         </FormProvider>
