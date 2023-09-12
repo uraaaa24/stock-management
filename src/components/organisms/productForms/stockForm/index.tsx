29bb2fbc98182839e4ad7849271ff6d6dd93251e
Yuto Urashima <yuto.urashima24@gmail.com>
2023-09-12 22:28:11 +0900

    stockFormをnumericForm使うようにした

--- src/components/organisms/productForms/stockForm/index.tsx
+++ src/components/organisms/productForms/stockForm/index.tsx
@@ -1,48 +1,11 @@
-import { InputLabel, TextField, Typography } from '@mui/material'
-import { Controller, useFormContext } from 'react-hook-form'
+import NumericForm from '../baseForms/numericForm'
 
 type StockFormProps = {
     name?: string
 }
 
 const StockForm = ({ name = 'stock' }: StockFormProps) => {
-    const { control } = useFormContext()
-
-    return (
-        <>
-            <Controller
-                name={name}
-                control={control}
-                rules={{
-                    required: { value: true, message: '入力してください' },
-                    pattern: {
-                        value: /^(([1-9]\d*)(\.\d+)?|0\.((0+[1-9]+)+|0*[1-9]+))$/,
-                        message: '数字を入力してください'
-                    }
-                }}
-                defaultValue=""
-                render={({ field, fieldState }) => {
-                    return (
-                        <>
-                            <InputLabel htmlFor={name}>
-                                <Typography variant="h5">個数</Typography>
-                            </InputLabel>
-                            <TextField
-                                {...field}
-                                id={name}
-                                type="text"
-                                variant="outlined"
-                                placeholder="10"
-                                error={fieldState.invalid}
-                                helperText={fieldState.error?.message}
-                                fullWidth={true}
-                            />
-                        </>
-                    )
-                }}
-            />
-        </>
-    )
+    return <NumericForm name={name} requiredMessage="商品個数を入力してください" label="商品個数" placeholder="100" />
 }
 
 export default StockForm
